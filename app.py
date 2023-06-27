1df723982c85e756169725cad3a42bf92e2376ca
Behdad Ehsani <behdadehsani1377@gmail.com>
2023-06-27 10:11:33 -0400

    v 1.1.0

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ openai.api_key = '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'
 app = Flask(__name__)
 
 questions = [
-    "Please tell me the job position:",
-    "Please tell me the job tasks:",
-    "Cloud Experience:",
-    "Programming Language Experience:",
-    "Database Experience:"
+    "Please tell me the job position you are hiring for:",
+    "Please provide a detailed description of the primary tasks and responsibilities for this position:",
+    "Please list the required skills and qualifications for this position (comma-separated):",
+    "Please specify the cloud platform/platforms required years of experience in working with cloud services:",
+    "Please indicate the required years of experience in programming languages:",
+    "Please specify the required years of experience in database management:",
+    "Please specify the required educational qualifications for this position:",
+    "Please provide some information about your company and its culture:",
+    "Please select the formality level of the job description response (formal, semi-formal, less formal):",
+    "Do you want to include information about company benefits? (yes/no):"
 ]
 
 answers = {}
@@ -27,15 +32,52 @@ def chat_with_hr():
             question_number += 1
             return render_template('chatbot.html', question=questions[question_number], question_number=question_number)
 
-        job_description = create_job_description(answers['0'], answers['1'], answers['2'], answers['3'], answers['4'])
-
-        return render_template('result.html', job_description=job_description)
+        if answers['9'].lower() == 'yes':
+            return render_template('benefits.html', question_number=question_number)
+        else:
+            job_description = create_job_description(answers['0'], answers['1'], answers['2'], answers['3'], answers['4'],
+                                                     answers['5'], answers['6'], answers['7'], answers['8'], None)
+            return render_template('result.html', job_description=job_description)
 
     return render_template('chatbot.html', question=questions[0], question_number=0)
 
 
-def create_job_description(position, tasks, cloud_exp, prog_langs, db_exp):
-    prompt = f"Create a job description for a {position}. The tasks for this role are: {tasks}. The required experience is as follows: {cloud_exp} years of experience in cloud services, {prog_langs} years of experience in the following programming languages, and {db_exp} years of experience in database management."
+@app.route('/benefits', methods=['POST'])
+def handle_benefits():
+    benefits_info = request.form.get('benefits_info')
+    job_description = create_job_description(answers['0'], answers['1'], answers['2'], answers['3'], answers['4'],
+                                             answers['5'], answers['6'], answers['7'], answers['8'], benefits_info)
+    return render_template('result.html', job_description=job_description)
+
+
+def create_job_description(position, tasks, skills_exp, cloud_exp, prog_langs, db_exp, education_req, company_info, formality_level, include_benefits):
+    skills_exp_list = skills_exp.split(",")
+    skills_exp_formatted = "\n- ".join(skills_exp_list)
+    skills_exp_prompt = f"The ideal candidate should possess the following skills and qualifications:\n- {skills_exp_formatted}."
+
+    cloud_exp_prompt = f"The candidate should have at least {cloud_exp} years of experience in working with cloud services."
+
+    prog_langs_prompt = f"The candidate should have expertise in programming languages with a minimum of {prog_langs} years of experience."
+
+    db_exp_prompt = f"The candidate should have {db_exp} years of experience in database management."
+
+    education_req_prompt = f"The candidate should have a {education_req} degree or equivalent educational background."
+
+    company_info_prompt = f"About the Company:\n\n{company_info}\n\n"
+
+    formality_prompt = ""
+    if formality_level == "formal":
+        formality_prompt = "This job description follows a formal tone."
+    elif formality_level == "semi-formal":
+        formality_prompt = "This job description has a semi-formal tone."
+    elif formality_level == "less formal":
+        formality_prompt = "This job description is less formal in tone."
+
+    benefits_prompt = ""
+    if include_benefits and include_benefits.lower() == "yes":
+        benefits_prompt = f"Company Benefits:\n\n{include_benefits}\n\n"
+
+    prompt = f"Create a job description for the position of {position}.\n\n{company_info_prompt}The primary tasks and responsibilities for this role include:\n{tasks}\n\n{skills_exp_prompt}\n\n{cloud_exp_prompt}\n\n{prog_langs_prompt}\n\n{db_exp_prompt}\n\n{education_req_prompt}\n\n{formality_prompt}\n\n{benefits_prompt}"
 
     response = openai.Completion.create(
         engine="text-davinci-003",
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import openai
 # Set up your OpenAI API credentials
 openai.api_key = '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'
 
+
 # Function to create job description
 def create_job_description(position, tasks, cloud_exp, prog_langs, db_exp):
     prompt = f"Create a job description for a {position}. The tasks for this role are: {tasks}. The required experience is as follows: {cloud_exp} years of experience in cloud services, {prog_langs} years of experience in the following programming languages, and {db_exp} years of experience in database management."
 
     response = openai.Completion.create(
-      engine="text-davinci-003", # Change this to the most recent model when GPT-4 is released.
-      prompt=prompt,
-      temperature=0.7,
-      max_tokens=300
+        engine="text-davinci-003",  # Change this to the most recent model when GPT-4 is released.
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=300
     )
 
     return response.choices[0].text.strip()
+
 
 # Interactive chat with HR
 def chat_with_hr():
@@ -29,5 +31,7 @@ def chat_with_hr():
     print("\nHere is the job description:\n")
     print(job_description)
 
+
 # Run the chatbot
 chat_with_hr()
+)
